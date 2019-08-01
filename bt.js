ed000da298e48a10db31b81f451401f36d0245db
Vladimir Varbanov <vlado@vlado1.com>
2019-08-01 23:34:23 +0300

    Connecting to Steam and trying to set an online status

--- bt.js
+++ bt.js
@@ -1,7 +1,14 @@
 const SteamUser = require('steam-user');
+const credentials = require('./config');
 const client = new SteamUser();
 
 const loginOptions = {
-  accountName: 'CTradeBot1',
-  password: ''
-}
+  accountName: credentials.username,
+  password: credentials.password,
+};
+
+client.logOn(loginOptions);
+client.on('loggedOn', () => {
+  console.log('logon success');
+  client.setPersona(SteamUser.Steam.EPersonaState.Online);
+});
